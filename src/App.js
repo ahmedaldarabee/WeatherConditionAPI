eb62cff5285df6b9ea4efb22ae985b2b1512d1a9
Ahmed Al Darabee <seahmednail@gmail.com>
2025-06-26 23:27:38 +0300

    translation file

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import * as React from "react"
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Container from '@mui/material/Container';
-import CloudOutlinedIcon from '@mui/icons-material/CloudOutlined';
 import CloudIcon from '@mui/icons-material/Cloud';
 import Button from '@mui/material/Button';
 import axios from "axios"
+import moment from 'moment/moment';
+import "moment/min/locales"
+import { useTranslation } from 'react-i18next';
 
+moment.locale("en")
 
 const theme = createTheme({
   typography: {
@@ -20,15 +23,81 @@ const theme = createTheme({
   },
 });
 
+let cancelAxios = null;
+
 function App() {
+
+  const [currentDate,setCurrentDate] = React.useState("");
+  const [temp , setTemp] = React.useState({
+    number: null,
+    description: "",
+    min: null,
+    max: null,
+    icon: null
+  });
+
+  const { t, i18n } = useTranslation();
+  const [locale , setLocale] = React.useState("ar");
+
+  // once you connect with external library, you will need to handle it by using useEffect to handle re-render section after each updating accrue in component!
+  const handleLanguageChange = () => {
+    if(locale === "ar"){
+      setLocale("en");
+      i18n.changeLanguage("en");
+      moment.locale("en")
+    }else{
+      setLocale("ar");
+      i18n.changeLanguage("ar");
+      moment.locale("ar")
+    }
+
+    // Why this state again here? where we do moment.locale("...") about each case? 
+    setCurrentDate(moment().format('MMMM Do YYYY'));
+  }
+
+  React.useEffect(() => {
+    
+    // بما اني بدياها تتحدث بشكل دوري ليش غلط احطها داخل ديبندنسي
+    setCurrentDate(moment().format('MMMM Do YYYY'));
+
+    // Make a request for a user with a given ID
+    axios.get('https://api.openweathermap.org/data/2.5/weather?lat=31.96&lon=35.93&appid=7190f37dec7d7579fed69f1e2076189f',{
+        cancelToken: new axios.CancelToken((cancel) => {
+          cancelAxios = cancel;
+        })
+      })
+      .then(function (response) {
+        const temp = Math.round(response.data.main.temp  - 272.15);
+        const description = response.data.weather[0].description;
+        const min = Math.round(response.data.main.temp_min  - 272.15);
+        const max = Math.round(response.data.main.temp_max  - 272.15);       
+        // const icon = Math.round(response.data.main.temp_max  - 272.15);       
+        const responseIcon = response.data.weather[0].icon;
+        
+        setTemp({
+          number: temp,
+          description,
+          min,
+          max,
+          icon: `https://openweathermap.org/img/wn/${responseIcon}@2x.png`,
+        })
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+      // What is the relationship with mounting and unmounting?
+      return () => {
+        cancelAxios();
+      }
+  },[]);
+
   return (
     <>
       <ThemeProvider theme={theme}>
         <Container maxWidth="sm">
-
             {/* Container Content */}
-            <div className='ContainerContent min-h-screen flex items-center justify-center'>
-
+            <div  dir={locale === "ar" ? 'rtl' : 'ltr'} className='ContainerContent min-h-screen flex items-center justify-center'>
               <div className='flex justify-center flex-col gap-3'>
                   <div className='cardBox bg-gray-300 my-4 p-4 rounded-lg shadow-xl cursor-pointer'>
                     <div className='cardContent'>
@@ -36,8 +105,8 @@ function App() {
                       <div className='cardData'>
                         <div className='cardCityTime my-4 flex items-end justify-center gap-3 flex-col'>
                             <div className='flex items-end justify-center gap-3'>
-                              <Typography variant="h3"> Amman </Typography>
-                              <Typography variant="h6"> sunday 26/06/2025 </Typography>
+                              <Typography variant="h3"> {t(`Amman`)} </Typography>
+                              <Typography variant="h6"> {currentDate} </Typography>
                             </div>
                             <hr className='bg-black w-full h-[2px]'/>
                         </div>
@@ -48,18 +117,23 @@ function App() {
                           
                           <div className='mainInfo'>
                               <div className='cardTemp flex items-center justify-start gap-3'>
-                                  <Typography variant="h5"> <span className='text-3xl'>20</span> c </Typography>
-                                  <Typography variant="h5"> <CloudOutlinedIcon/> </Typography>
+                                  <Typography variant="h2"> {temp.number} </Typography>
+                                  {temp.icon ? (
+                                  <img src={temp.icon} alt="weather icon" />
+                                  ) : (
+                                    <p> weather icon...</p>
+                                  )}
+
                               </div>
 
                               <div className='cardCityStatus'>
-                                  <Typography variant="h6"> broken clouds </Typography>
+                                  <Typography variant="h6"> {t(temp.description)} </Typography>
                               </div>
 
                               <div className='flex items-center justify-start gap-3'>
-                                  <h5> <span> min: </span> 15 </h5>
+                                  <h5> <span> {t(`min`)}: </span> {temp.min} </h5>
                                     <div className='w-[2px] h-5 bg-black'></div>
-                                  <h5> <span> max: </span> 30 </h5>
+                                  <h5> <span> {t(`max`)}: </span>  {temp.max} </h5>
                               </div>
                           </div>
 
@@ -68,16 +142,14 @@ function App() {
                           </div>
 
                       </div>
-                    
+
                     </div>
                   </div>
                   <div className='langBtn'>
-                    <Button variant="text"> <span className='capitalize text-black'>English</span> </Button>
+                    <Button variant="text" onClick={handleLanguageChange}> <span className='capitalize text-black'> {locale === "ar"? "Arabic":"English"} </span> </Button>
                   </div>
               </div>
             </div>
-
-
         </Container>
       </ThemeProvider>
     </>
