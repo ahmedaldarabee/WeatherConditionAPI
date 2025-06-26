83ea2c3d5cad0b03fb399f8b118cddd41fc19097
Ahmed Al Darabee <seahmednail@gmail.com>
2025-06-26 14:49:52 +0300

    build ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Container from '@mui/material/Container';
+import CloudOutlinedIcon from '@mui/icons-material/CloudOutlined';
+import CloudIcon from '@mui/icons-material/Cloud';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["UbuntuRegular"],
+  },
+  palette: {
+    primary: {
+      main: "#039be5",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <Container maxWidth="sm">
+            {/* Container Content */}
+            <div className='ContainerContent min-h-screen flex items-center justify-center'>
+              <div className='cardBox bg-gray-300 p-4 rounded-lg shadow-inner cursor-pointer'>
+                <div className='cardContent'>
+                    {/* header */}
+                  <div className='cardData'>
+                    <div className='cardCityTime my-4 flex items-end justify-center gap-3 flex-col'>
+                        <div className='flex items-end justify-center gap-3'>
+                          <Typography variant="h3"> Amman </Typography>
+                          <Typography variant="h6"> sunday 26/06/2025 </Typography>
+                        </div>
+                        <hr className='bg-black w-full h-[2px]'/>
+                    </div>
+                  </div>
+
+                  {/* body */}
+                  <div className='cardDegreeInfo flex items-center justify-around'>
+                      
+                      <div className='mainInfo'>
+                          <div className='cardTemp flex items-center justify-start gap-3'>
+                              <Typography variant="h5"> <span className='text-3xl'>20</span> c </Typography>
+                              <Typography variant="h5"> <CloudOutlinedIcon/> </Typography>
+                          </div>
+
+                          <div className='cardCityStatus'>
+                              <Typography variant="h6"> broken clouds </Typography>
+                          </div>
+
+                          <div className='flex items-center justify-start gap-3'>
+                              <h5> <span> min: </span> 15 </h5>
+                                <div className='w-[2px] h-5 bg-black'></div>
+                              <h5> <span> max: </span> 30 </h5>
+                          </div>
+                      </div>
+
+                      <div className='cloudImg'>
+                        <CloudIcon className='text-white' style={{fontSize:"150px"}}/>
+                      </div>
+
+                  </div>
+                
+                </div>
+              </div>
+            </div>
+        </Container>
+      </ThemeProvider>
+    </>
   );
 }
 
